2ac23eb047dc830fdf5eb7687dd101a0cf77fe42
Mac <mac.kittipat@gmail.com>
2024-01-06 14:25:20 +0700

    Add axios lib

--- index.js
+++ index.js
@@ -1,13 +1,14 @@
+const axios = require('axios');
+
 module.exports.handler = async (event) => {
-  return {
-    statusCode: 200,
-    body: JSON.stringify(
-      {
-        message: "Go Serverless v3.0! Your function executed successfully!",
-        input: event,
-      },
-      null,
-      2
-    ),
-  };
+    console.log('Running ...');
+    let response = null;
+    try {
+        response = await axios.get('https://pokeapi.co/api/v2/pokemon/ditto');
+        console.log(response.data);
+    } catch (error) {
+        console.error(error);
+    }
+
+    return JSON.stringify(response.data)
 };
